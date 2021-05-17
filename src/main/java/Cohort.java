9a61ae33d9167e552d470af4d1d46e8dbd6c57f6
Emmanuel Stephen <emmanuel.r.stephen86@gmail.com>
2021-05-17 09:51:24 -0500

    Feat: cohoart test added

--- src/main/java/Cohort.java
+++ src/main/java/Cohort.java
@@ -2,6 +2,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Cohort {
-    public List<Student> students;
 
+    private List<Student> students;
+
+    public Cohort() {
+        this.students = new ArrayList<>();
+    }
+
+    public double getCohortAverage() {
+        double avg = 0;
+        for (Student student: this.getStudents()) {
+            avg += student.getGradeAverage();
+        }
+        return avg / this.getStudents().size();
+    }
+
+    public void addStudent(Student student){
+        students.add(student);
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
 }
